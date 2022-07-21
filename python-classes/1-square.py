ef0eb209d88575aede73570a7d3f5c40a02f8d6a
buterajacques1 <jbutera702@gmail.com>
2022-07-21 10:53:57 -0700

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+MODULE IS DOCUMENTED
+"""
+
+
+class Square:
+    """
+    CLASS FORMS PRIVATE INSTANCE
+    """
+    def __init__(self, size):
+        self._Square__size = size
