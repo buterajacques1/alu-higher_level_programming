97a472f123aa2d7c4c232539ae28d50968de20e9
buterajacques1 <jbutera702@gmail.com>
2022-07-29 06:19:08 -0700

    cURL only methods

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sX DELETE "$1"
+# URL that displays all HTTP
+curl -sI "$1" | grep "Allow: " | sed 's/Allow: //'
