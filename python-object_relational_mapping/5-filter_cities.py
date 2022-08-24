f87c1414813906328dfc24ed6ad6598707c1585b
buterajacques1 <jbutera702@gmail.com>
2022-08-24 11:13:00 -0700

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,3 +14,5 @@ if __name__ == '__main__':
     FROM cities JOIN states ON cities.state_id = states.id \
     WHERE states.name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
+
+    print(", ".join([state[1] for state in states]))
