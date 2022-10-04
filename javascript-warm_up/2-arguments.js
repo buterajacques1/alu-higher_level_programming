675900b74b58542075fc55e0a2fc3abd1ab1d875
buterajacques1 <jbutera702@gmail.com>
2022-10-04 02:24:22 -0700

    Argumens

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.arg.lenth < 3){
+	console.log('No argument');
+}
+else if (process.arg.lenth === 3){
+	console.log('Arguments found');
+}
+else{
+	console.log('Argumenrs found');
+}
