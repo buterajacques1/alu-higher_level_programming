0ef45a7b164264a1b127a0ed865fd0e8b3bd682b
buterajacques1 <jbutera702@gmail.com>
2022-10-19 11:55:20 -0700

    Number conversion

---
+++ javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,9 @@
+exports.converter = function (base) {
+  return (num) => {
+    return num.toString(base);
+  };
+};exports.converter = function (base) {
+  return (num) => {
+    return num.toString(base);
+  };
+};
